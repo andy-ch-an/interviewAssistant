2c40d65443533bb6c367c9d1dfb52de90d195f12
Calvin Wiogo <calvinwiogo2808@gmail.com>
2023-01-09 17:26:46 +0800

    vid progress

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Loader from "./component/Loader"
 import EmotionLineChart from "./component/EmotionLineChart"
 import Instruction from "./component/Instruction";
 import CountDown from "./component/CountDown";
-import OutputRecord from "./component/outputRecord";
 
 function App() {
   return(
@@ -21,7 +20,6 @@ function App() {
           <Route exact path="/Record" element={<Record />}/>    
           <Route exact path="/Record" element={<Instruction/>}/> 
           <Route exact path="/Count" element={<CountDown />}/> 
-          <Route exact path="/OutputRecord" element={<OutputRecord />} />
         </Routes>
       </Router>
       {/* <Loader></Loader> */}
